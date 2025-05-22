9cb1797738e106effec6d9b0a505844134389aed
FlamingBun <wh180911@gmail.com>
2025-05-22 18:16:37 +0900

    feat: 플레이어 이동 구현

---
+++ Assets/00_Scripts/CharacterManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour
+{
+    private Player player;
+    public Player Player { get { return player; } set { player = value; } }
+}
